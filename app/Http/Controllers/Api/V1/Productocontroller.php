b0dde60250ca9e2c5ec06a0000c5aa88bef088c0
Samir Delvalle <valortsaga@gmail.com>
2024-08-27 23:11:01 -0500

    creacion de Api Productos

---
+++ app/Http/Controllers/Api/V1/Productocontroller.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class Productocontroller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Producto $producto)
+    {
+        //
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('titulo','Minimercado')</title>
+</head>
+<body>
+    <header>
+        {{--navbar--}}
+        @include('layouts.navbar')
+    <header>
+    <main>
+        @yield('contenido')
+    <main>
+    <footer>
+        @include('layouts.footer')
+    <footer>
+</body>
+</html>
